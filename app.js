1979a7b870d85cce0e71b63e80ce2b9ded57d1e6
unknown <sharper.ag@mail.ru>
2021-08-26 15:27:04 +0500

    changed js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     "use strict";
 }()); */
 
-const numberOfFilms = +prompt("How many movies have you seen?");
+/* const numberOfFilms = +prompt("How many movies have you seen?");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,7 +11,6 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
 
 const lastMovie = prompt("One of the last movies you have seen?");
 const ratingOfMovie = +prompt("What  raiting  would you give to movie?");
@@ -23,4 +22,27 @@ personalMovieDB.movies[lastMovie] = ratingOfMovie;
 
 personalMovieDB.actors[actor] = role;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
+
+
+const dishName = prompt("What's your favorite dish?");
+
+const recipesTable = {
+        name: dishName,
+        ingredients:[],
+        recipe: {},
+        typeOfDish: ""
+};
+
+const numberOfRecipe = +prompt("Number of recipe");
+const recipe = prompt("What's the recipe dish?");
+
+
+const ingridi = ["potato","tomato", "chicken","pepper"];
+
+recipesTable.ingredients = ingridi;
+recipesTable.recipe[numberOfRecipe] = recipe;
+
+
+console.log(recipesTable);
+
